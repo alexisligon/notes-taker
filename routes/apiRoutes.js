f2d143e620096272aa44393ec9c06de3c0064707
alexisligon <aligo@macbook-pro.home>
2021-06-15 22:19:26 -0400

    added comments to code, started delete code

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,13 +1,7 @@
-// LOAD DATA
-// We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
 const path = require('path');
 const notesData = require('../db/db.json');
 const fs = require('fs');
-const { v4: uuidv4 } = require ('uuid');
-
-
-
+const { v4: uuidv4 } = require('uuid');
 
 // ROUTING
 
@@ -15,20 +9,26 @@ module.exports = (app) => {
 
   app.get('/api/notes', (req, res) => res.json(notesData));
 
-  app.get("/api/notes/:id", (req, res) => {
-    res.json(notesData[Number(req.params.id)])})
-
   app.post('/api/notes', (req, res) => {
-    
+
+    //push data to database
     notesData.push(req.body);
     console.log(req.body);
+
+    //run unique id code for the id of the body
     req.body.id = uuidv4();
     console.log('id: ', req.body.id)
-     
 
-    fs.writeFile(path.join(__dirname, '../db/db.json'), JSON.stringify(notesData), function(err){
+    //write the file of the database, stringify for json format
+    //if error, throw error
+    fs.writeFile(path.join(__dirname, '../db/db.json'), JSON.stringify(notesData), function (err) {
       if (err) throw (err);
       res.json(true);
     })
 
-  })}
+  })
+
+  app.delete('/api/notes/:id', (req, res) => {
+    //put delete code here!!!
+  })
+}
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,10 +5,12 @@ const path = require('path');
 
 module.exports = (app) => {
 
+  //get notes html for notes query
   app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/notes.html'));
   });
 
+  //if no query, get index.html
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
   });
